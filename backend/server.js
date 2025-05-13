38b7951508c0a8cd6f481c0fe0ba7a225ab7a7e3
Sejalmohitkar <sejalmohitkar2002gmail.com>
2025-05-13 15:52:17 +0530

    add bakend

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
+
+app.get("/jokes", (req, res) => {
+    const jokes=[
+        {
+          id:1,
+          title:"A joke 1",
+          content: "This is my joke",
+        },
+         {
+          id:2,
+          title:"A joke 2",
+          content: "This is my joke",
+        },
+
+         {
+          id:3,
+          title:"A joke 3",
+          content: "This is my joke",
+        },
+
+         {
+          id:4,
+          title:"A joke 4",
+          content: "This is my joke",
+        },
+    ];
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
+
+    
